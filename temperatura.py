f3ba7cb6de01d7bb2cc237a8c910fbcf80f8acad
kku2020 <kkurodrigues1996@gmail.com>
2021-02-23 15:04:20 -0300

    Arquivos e Projetos do Vamo AI

---
+++ temperatura.py
@@ -0,0 +1,8 @@
+temperatura = float(input("Qual é a sua temperatura?"))
+
+if temperatura > 37:
+    print("Sinto muito, você está com febre, melhor tomar um antitérmico!")
+else:
+    print("Você não está com febre volte para casa!")
+
+ 
---
+++ vidente.py
@@ -0,0 +1,13 @@
+nome = input("Qual é o seu nome")
+print (nome)
+
+numero_de_filhos = input("Quanos filhos você deseja ter?")
+print (numero_de_filhos)
+
+cidade = input("Em qualcidade você gostaria de morar?")
+print (cidade)
+
+profissao = input("Qual sua profissão dos sonhos?")
+print (profissao)
+
+print ("A vidente preve que", nome, "tera", numero_de_filhos,"filhos e vivera em", cidade, "trabalhando como", profissao, "em 2023")
